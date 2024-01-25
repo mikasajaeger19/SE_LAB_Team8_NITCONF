3b1dca0e87886f494041686e0cd1595338c3a113
mikasajaeger19 <naeemniaz2012@gmail.com>
2024-01-25 15:58:32 +0530

    author controller fixed

--- nitconf/src/main/java/nitconf/nitconf/controllers/AuthorController.java
+++ nitconf/src/main/java/nitconf/nitconf/controllers/AuthorController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import nitconf.nitconf.models.Author;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/authors")
@@ -15,16 +17,10 @@ public class AuthorController{
        return new Author("C1", "Rafael", "rafael@gmail.com", "999999999");
        
    }
-}
-
-@RestController
-@RequestMapping("/authors")
-public class AuthorControllerTest{
 
    @GetMapping("/hello")
-   public String getAuthor(){
-
-       return "Hello World";
-       
+   public String hello() {
+       return "Hello world!";
    }
+   
 }
