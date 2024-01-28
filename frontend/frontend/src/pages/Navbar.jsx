2a6604ceccc977b08d420c0fc798e148d1d59ab2
Madhav-Murali-NITC <madhav_b210027cs@nitc.ac.in>
2024-01-28 14:43:03 +0530

    frontend w dummy values (add api calls later)

--- frontend/frontend/src/pages/Navbar.jsx
+++ frontend/frontend/src/pages/Navbar.jsx
@@ -8,13 +8,13 @@ function Navbar() {
           <Link to="/personal">Personal Info</Link>
         </h3>
         <h3 className="nav--dashboard">
-          <Link to="/home">Dashboard</Link>
+          <Link to="/dashboard">Dashboard</Link>
         </h3>
         <h3 className="nav--submit">
-          <Link to="/cart">Submit Abstract</Link>
+          <Link to="/submit">Submit Abstract</Link>
         </h3>
         <h3 className="nav--reupload">
-          <Link to="/account">Account</Link>
+          <Link to="/reupload">Account</Link>
         </h3>
       </nav>
     );
