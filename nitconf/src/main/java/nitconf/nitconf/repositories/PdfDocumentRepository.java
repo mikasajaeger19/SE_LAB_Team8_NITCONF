665ebf62927ded7c1f9518497bbd1888296b79e9
mikasajaeger19 <naeemniaz2012@gmail.com>
2024-02-13 21:03:40 +0530

    removal of extra folder

--- nitconf/src/main/java/nitconf/nitconf/repositories/PdfDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package nitconf.nitconf.repositories;
-
-import nitconf.nitconf.models.PdfDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PdfDocumentRepository extends JpaRepository<PdfDocument, Long> {
-}
